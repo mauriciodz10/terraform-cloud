4eae22c1fe0be28855af6ff1c0a2e78f7996f8ab
Mauricio Diaz <mauriciodiazr10@gmail.com>
2023-09-10 19:12:53 -0500

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "sori_corp"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
